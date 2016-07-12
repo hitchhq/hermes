2b9364943054f01cc6fc6a932db45339b4084483
fmvilas <fmvilas@gmail.com>
2016-07-12 17:16:11 +0200

    Move socketio to its correct file

--- clients/socketio.js
+++ clients/socketio.js
@@ -1,26 +1,71 @@
-const path = require('path');
-const app = require('http').createServer(handler);
-const io = require('socket.io')(app);
-const fs = require('fs');
+const socketio = require('socket.io');
 
-app.listen(80);
+function init (settings) {
+  return function (hermes) {
+    return new HermesSocketIO(settings, hermes);
+  };
+}
 
-function handler (req, res) {
-  fs.readFile(path.resolve(__dirname, 'index.html'),
-  (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+function HermesSocketIO (settings, hermes) {
+  this.hermes = hermes;
+  this.server_settings = settings;
+  this.listen = _listen.bind(this);
+  this.send = _send.bind(this);
+}
 
-    res.writeHead(200);
-    res.end(data);
+function _setup () {
+  this.http_server = this.server_settings.http_server;
+  if (!this.http_server) {
+    this.io = socketio();
+  } else {
+    this.io = socketio(this.http_server);
+  }
+
+  this.io.on('connection', (socket) => {
+    this.hermes.emit('client:ready', { name: 'Socket.IO adapter' });
+    socket.onevent = (packet) => {
+      const topic = packet.data[0];
+      _published.call(this, _createMessageFromClient.call(this, topic, packet, socket));
+    };
   });
 }
 
-io.on('connection', (socket) => {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', (data) => {
-    console.log(data);
-  });
-});
+function _listen () {
+  _setup.call(this);
+  if (this.http_server.listen) {
+    this.http_server.listen(this.server_settings.port || 80);
+  } else {
+    this.http_server(this.server_settings.port || 80);
+  }
+  return this.client;
+}
+
+function _published (message) {
+  this.hermes.emit('client:message', message);
+}
+
+function _createMessage (topic, message) {
+  return {
+    protocol: message.protocol || this.server_settings.protocol || 'ws',
+    payload: message.payload,
+    topic,
+    headers: message.headers
+  };
+}
+
+function _createMessageFromClient (topic, packet, client) {
+  return {
+    protocol: 'ws',
+    payload: packet.data[1],
+    topic,
+    headers: {},
+    ws_client: client,
+    original_packet: packet
+  };
+}
+
+function _send (message) {
+  this.io.emit(message.topic, _createMessage.call(this, message.topic, message));
+}
+
+module.exports = init;
