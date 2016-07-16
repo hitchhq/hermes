e2b73a0eef36d9c2b99abe795bf0b2f7d1a3609c
fmvilas <fmvilas@gmail.com>
2016-07-16 12:50:49 +0200

    Fix example

--- examples/routes/hello.js
+++ examples/routes/hello.js
@@ -1,4 +1,4 @@
-const router = require('hermes-router')();
+const router = require('hermesjs-router')();
 
 router.in.client.use(':name', (message, next) => {
   console.log(`Hello from Client ${message.route.params.name}!`);
